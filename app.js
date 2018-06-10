e585ab482f4f3c0d034905334dbfea126e01a346
Chris Ivey <chrisivey01@gmail.com>
2018-06-10 17:55:18 -0500

    ready

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const router = express.Router();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -12,15 +18,18 @@ const accountSid = 'AC094faa1b8edda9e300224ef60ae8e47a';
 const authToken = '9266229c7ef57aee437643a5f7d0087b';
 const client = require('twilio')(accountSid, authToken);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Content-Type', 'application/json');
 
-    next();
-});
 
-router.get('/', function(req, res) {
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'windowwasher1',
+    database: 'todo'
+})
+
+router.get('/', function(req, res, next) {
 
     client.messages
         .create({
@@ -31,7 +40,39 @@ router.get('/', function(req, res) {
         .then(message => console.log(message.sid))
 
    res.send(JSON.stringify({  message : "success!", success:true}))
+
 });
+ 
+app.get('/load', function(req,res,next){
+
+        connection.query('SELECT * FROM todo ', function(err, results) {
+            if (err) throw err;
+            res.send((results));
+        })
+    });
+
+
+
+app.post('/send', function(req,res,next){
+        var post = req.body;
+        connection.query("INSERT INTO todo SET ?",
+            post, function(err, results, fields) {
+            if (err) throw err;
+            console.log(results.insertId);
+            res.end(JSON.stringify(results));
+        })
+    });
+
+app.delete('/delete/:id', function(req,res,next) {
+    var post = req.params;
+    connection.query('DELETE FROM `todo` WHERE `id`=?', [post.id],
+        function (err, results, fields) {
+            if (err) throw err;
+            res.end('Record has been deleted!');
+        })
+})
+
+
 
 
 app.use(router);
